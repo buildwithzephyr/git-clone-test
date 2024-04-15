4aa646d6e7e977aff20c07f786ac7f431a8b07d4
Jordan Rickman <jordan.rickman.42+zephyr@gmail.com>
2024-04-14 17:10:35 -0700

    Gitignore js files; build to dist/

--- subDir/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
-function handler(evt) {
-    return { statusCode: 200, status: "this is a branch" };
-}
-exports.handler = handler;
